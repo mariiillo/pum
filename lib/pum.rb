70c0d9c90cc3bfdcabbc8b1028f265260a60de88
Mario Viapiano <marioeviapiano@gmail.com>
2019-09-17 22:03:13 +0200

    Add pum/pum.rb to contain the "core" module.

--- lib/pum.rb
+++ lib/pum.rb
@@ -1,22 +1,3 @@
 require "pum/version"
 require "pum/configure"
-
-module Pum
-  ParamNotFound = Class.new(StandardError)
-
-  def self.included(base)
-    base.extend(ClassMethods)
-  end
-
-  module ClassMethods
-    def call(param)
-      new(param).call
-    end
-  end
-
-  private def initialize(param)
-    raise ParamNotFound if param.nil?
-
-    @param = param
-  end
-end
+require "pum/pum"
---
+++ lib/pum/pum.rb
@@ -0,0 +1,19 @@
+module Pum
+  ParamNotFound = Class.new(StandardError)
+
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def call(param)
+      new(param).call
+    end
+  end
+
+  private def initialize(param)
+    raise ParamNotFound if param.nil?
+
+    @param = param
+  end
+end
